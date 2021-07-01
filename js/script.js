d03ce61637c360b4ab3698f34d1eb44646d8bc4d
Sophie <sophiehickey123@gmail.com>
2021-07-01 18:26:01 +0100

    fetched data from json to display on web

---
+++ js/script.js
@@ -0,0 +1,49 @@
+
+
+document.querySelector('.button-container').addEventListener("click", () => {
+    let text = document.getElementById('filter-jobs').value;
+})
+
+function getJobs(){
+    return fetch("data.json")
+    .then(response => response.json())
+    .then(data => {
+        return data
+    })
+}
+
+function showJobs(jobs){
+    console.log(jobs);
+    let jobsContainer = document.querySelector(".jobs-container");
+    let jobsHTML = "";
+    jobs.forEach(job => {
+        jobsHTML += `
+            <div class="job-title">
+                <div class="top">
+                    <img src="${job.logo}">
+                    <span class="material-icons more_horiz">more_horiz</span>
+                </div>
+                <div class="rolename">
+                    <span>${job.roleName}</span>
+                </div>
+                <div class="description">
+                    <span>${job.requirements.content}</span>
+                </div>
+                <div class="buttons">
+                    <div class="button apply-now">
+                        Apply Now
+                    </div>
+                    <div class="button">
+                        Message
+                    </div>
+                </div>
+            </div>      
+        
+        `
+    })
+    jobsContainer.innerHTML = jobsHTML;
+}
+
+getJobs().then(data => {
+    showJobs(data);
+})
